eeb79b0e0c567d56228dfbf969c56723276567b4
颜风 <yanfengstyle@163.com>
2015-07-19 02:32:22 +0800

    Initial Commit

---
+++ Yan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yan
+//
+//  Created by 颜风 on 15/7/19.
+//  Copyright (c) 2015年 颜风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Yan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Yan
+//
+//  Created by 颜风 on 15/7/19.
+//  Copyright (c) 2015年 颜风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
